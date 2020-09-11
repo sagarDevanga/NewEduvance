a66c1ead860ee137e5a565796f35627ad20c3abf
Sagar Devanga <sagardevanga@gmail.com>
2020-09-11 09:40:01 +0530

    Navigation set
    Navigation has been set using the STACK NAVIGATOR and DRAWER NAVIGATOR

--- App.js
+++ App.js
@@ -1,37 +1,19 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {Button, View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import AttendeeDetails from './containers/List/AttendeeList';
+import MyDrawer from './containers/MyDrawer';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyDrawer} />
+        <Stack.Screen name="AttendeeDetails" component={AttendeeDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
